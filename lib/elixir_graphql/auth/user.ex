a697e2ba5e2463056a3c6d25ed9f0377987fbc51
richie <connecttorichie@gmail.com>
2025-01-27 23:26:11 +0530

    Hashing password

--- lib/elixir_graphql/auth/user.ex
+++ lib/elixir_graphql/auth/user.ex
@@ -18,8 +18,16 @@ defmodule ElixirGraphql.Auth.User do
     |> validate_required([:name, :email, :username, :password])
     |> unique_constraint(:username)
     |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
     |> validate_length(:username, min: 4, max: 30)
     |> validate_length(:name, min: 3, max: 30)
     |> validate_length(:password, min: 8, max: 30)
+    |> hash_password()
   end
+
+  defp hash_password(%Ecto.Changeset{valid?: true} = changeset),
+    do: put_change(changeset, :password, Argon2.hash_pwd_salt(get_change(changeset, :password)))
+
+  defp hash_password(changeset), do: changeset
 end
