92bf2e18c4fe1aca50e9e1daa3b6a68df0c293b7
richie <connecttorichie@gmail.com>
2025-01-28 20:00:13 +0530

    Added more helper constants.

--- lib/elixir_graphql_web/constants/constants.ex
+++ lib/elixir_graphql_web/constants/constants.ex
@@ -1,4 +1,10 @@
 defmodule ElixirGraphqlWeb.Constants do
   @internal_server_error "Internal Server Error!"
+  @invalid_credentials "Invalid Credentials!"
+  @not_authenticated "Not Authenticated!"
+  @not_authorized "Not Authorized!"
   def internal_server_error, do: @internal_server_error
+  def invalid_credentials, do: @invalid_credentials
+  def not_authenticated, do: @not_authenticated
+  def not_authorized, do: @not_authorized
 end
