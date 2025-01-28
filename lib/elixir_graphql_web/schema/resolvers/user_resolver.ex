20389430ff7e21dff3e2f84c1de52bf81a7f823c
richie <connecttorichie@gmail.com>
2025-01-28 16:32:08 +0530

    removed register user api in graphql

--- lib/elixir_graphql_web/schema/resolvers/user_resolver.ex
+++ lib/elixir_graphql_web/schema/resolvers/user_resolver.ex
@@ -1,22 +1,7 @@
 defmodule ElixirGraphqlWeb.Schema.Resolvers.UserResolver do
   alias ElixirGraphql.Auth
-  alias ElixirGraphqlWeb.Utils
-  alias ElixirGraphqlWeb.Constants
 
   def get_all_users(_, _, _) do
     {:ok, Auth.list_users()}
-  end
-
-  def register_user(_, %{input: input}, _) do
-    case Auth.create_user(input) do
-      {:ok, _user} ->
-        {:ok, true}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:error, Utils.format_changeset_errors(changeset)}
-
-      _ ->
-        {:error, Constants.internal_server_error()}
-    end
   end
 end
